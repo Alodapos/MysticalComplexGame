f0f5ddac5f4fbb6575e3462fe27761114bebb4de
Alodapos <kazi666@hotmail.gr>
2015-03-12 16:54:40 +0200

    a test item

---
+++ src/ZorkGame/Items/AnotherTestItem.java
@@ -0,0 +1,62 @@
+package ZorkGame.Items;
+
+/**
+ * Created by sakis on 12-Mar-15.
+ */
+public class AnotherTestItem implements IItem {
+    private String name;
+    private String description;
+    private boolean canBeUsed;
+    private boolean canBePicked;
+
+    public AnotherTestItem(){
+        name = "a test item";
+        description= "loooook at me i'm an item!!";
+        canBeUsed = false;
+        canBePicked = true;
+    }
+
+    @Override
+    public boolean isCanBePicked() {
+        return canBePicked;
+    }
+
+    @Override
+    public void setCanBePicked(boolean canPick) {
+        this.canBePicked = canPick;
+    }
+
+    @Override
+    public boolean isUseable() {
+        return canBeUsed;
+    }
+
+    @Override
+    public void setUseable(boolean canUse) {
+        this.canBeUsed = canUse;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private void useItem (){
+        //TODO on-use effect
+    }
+}
