9322d81f7bd81cb78d2c1454b2d1e11fbd96fa72
Xelion7 <kounabi7@hotmail.com>
2015-05-29 12:28:49 +0300

    Fancy a pebble? Ο.ο

---
+++ src/MysticalComplexGame/Items/PinkPebble.java
@@ -0,0 +1,12 @@
+package MysticalComplexGame.Items;
+
+public class PinkPebble extends IItem
+{
+    public PinkPebble()
+    {
+        name = "pink pebble";
+        description = "A not-so-uncommon pink pebble is sunken into the fountain's water.";
+        inventoryDescription = "A pink pebble. ~I prefer lucky coins!~";
+        pickable = true;
+    }
+}
---
+++ src/MysticalComplexGame/Items/TurquoisePebble.java
@@ -0,0 +1,12 @@
+package MysticalComplexGame.Items;
+
+public class TurquoisePebble extends IItem
+{
+    public TurquoisePebble()
+    {
+        name = "turquoise pebble";
+        description = "A not-so-uncommon turquoise pebble is sunken into the fountain's water.";
+        inventoryDescription = "A turquoise pebble. ~I prefer lucky coins!~";
+        pickable = true;
+    }
+}
